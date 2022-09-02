510ca91f2c144c43f494b988cf811af3cfedf6d5
Sorikairo <sorikairox@gmail.com>
2022-09-02 16:07:14 +0900

    feat: nested validation for object

--- errors.ts
+++ errors.ts
@@ -1,6 +1,13 @@
 import { constraintKey } from './validate.ts';
 import { ValidateFunctionOptions } from './types.ts';
 
+export type ValidationError = {
+	property: string;
+	errorMessage: string;
+	// deno-lint-ignore no-explicit-any
+	constraints: any[];
+};
+
 export const createErrorMessage = (options?: ValidateFunctionOptions) => {
 	let message = options?.errorMessage || 'Failed behavior';
 	if (options?.constraints) {
@@ -12,4 +19,8 @@ export const createErrorMessage = (options?: ValidateFunctionOptions) => {
 		}
 	}
 	return message;
+};
+
+export const formatErrors = (errors: ValidationError[], propertyName: string) => {
+	return errors.map((error: ValidationError) => ({ ...error, property: `${propertyName}.${error.property}` }));
 };
