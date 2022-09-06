fd9da4b155ed85b97a4c56473042c9592aef406b
Sorikairo <sorikairox@gmail.com>
2022-09-06 16:58:32 +0900

    feat: isInternationalPhoneNumber validator

--- spec/validators/phone-number.test.ts
+++ spec/validators/phone-number.test.ts
@@ -1,38 +1,84 @@
 import { assertEquals } from 'https://deno.land/std@0.135.0/testing/asserts.ts';
 import { validateObject } from '../../validate.ts';
-import { IsPhoneNumber } from '../../validators/phone-number.ts';
+import { IsInternationalPhoneNumber, IsPhoneNumber } from '../../validators/phone-number.ts';
+import TestContext = Deno.TestContext;
 
 class UserWithPhoneNumber {
 	@IsPhoneNumber('FR')
 	phoneNumber!: string;
 }
 
-Deno.test('@IsLocalPhoneNumber with wrong phonenumber', () => {
-	const user = new UserWithPhoneNumber();
-	user.phoneNumber = 'werweqr';
-	assertEquals(validateObject(user, UserWithPhoneNumber), [
-		{
-			property: 'phoneNumber',
-			errorMessage: `String is not a local phone number from FR`,
-			constraints: ['FR'],
-		},
-	]);
+Deno.test('IsPhoneNumber with argument', async (ctx: TestContext) => {
+	await ctx.step('wrong phonenumber', () => {
+		const user = new UserWithPhoneNumber();
+		user.phoneNumber = 'werweqr';
+		assertEquals(validateObject(user, UserWithPhoneNumber), [
+			{
+				property: 'phoneNumber',
+				errorMessage: `String is not a local phone number from FR`,
+				constraints: ['FR'],
+			},
+		]);
+	});
+
+	await ctx.step('local phone number', () => {
+		const user = new UserWithPhoneNumber();
+		user.phoneNumber = '0789610921';
+		assertEquals(validateObject(user, UserWithPhoneNumber), []);
+	});
+
+	await ctx.step('international phone number', () => {
+		const user = new UserWithPhoneNumber();
+		user.phoneNumber = '+33789610921';
+		assertEquals(validateObject(user, UserWithPhoneNumber), []);
+	});
+
+	await ctx.step('international phone number with leading 0', () => {
+		const user = new UserWithPhoneNumber();
+		user.phoneNumber = '+330789610921';
+		assertEquals(validateObject(user, UserWithPhoneNumber), []);
+	});
 });
 
-Deno.test('@IsLocalPhoneNumber with local phone number', () => {
-	const user = new UserWithPhoneNumber();
-	user.phoneNumber = '0789610921';
-	assertEquals(validateObject(user, UserWithPhoneNumber), []);
-});
+class UserWithInternationalPhoneNumber {
+	@IsInternationalPhoneNumber()
+	phoneNumber!: string;
+}
 
-Deno.test('@IsLocalPhoneNumber with international phone number', () => {
-	const user = new UserWithPhoneNumber();
-	user.phoneNumber = '+33789610921';
-	assertEquals(validateObject(user, UserWithPhoneNumber), []);
-});
+Deno.test('IsPhoneNumber without argument', async (ctx: TestContext) => {
+	await ctx.step('Wrong phonenumber', () => {
+		const user = new UserWithInternationalPhoneNumber();
+		user.phoneNumber = 'werweqr';
+		assertEquals(validateObject(user, UserWithInternationalPhoneNumber), [
+			{
+				property: 'phoneNumber',
+				errorMessage: `String is not an international phone number`,
+				constraints: [],
+			},
+		]);
+	});
 
-Deno.test('@IsLocalPhoneNumber with international phone number with leading 0', () => {
-	const user = new UserWithPhoneNumber();
-	user.phoneNumber = '+330789610921';
-	assertEquals(validateObject(user, UserWithPhoneNumber), []);
+	await ctx.step('local phone number', () => {
+		const user = new UserWithInternationalPhoneNumber();
+		user.phoneNumber = '0789610921';
+		assertEquals(validateObject(user, UserWithInternationalPhoneNumber), [
+			{
+				property: 'phoneNumber',
+				errorMessage: `String is not an international phone number`,
+				constraints: [],
+			},
+		]);
+	});
+
+	await ctx.step('international phone number', () => {
+		const user = new UserWithInternationalPhoneNumber();
+		user.phoneNumber = '+33789610921';
+		assertEquals(validateObject(user, UserWithInternationalPhoneNumber), []);
+	});
+
+	await ctx.step('international phone number with leading 0', () => {
+		const user = new UserWithInternationalPhoneNumber();
+		user.phoneNumber = '+330789610921';
+		assertEquals(validateObject(user, UserWithInternationalPhoneNumber), []);
+	});
 });
--- validators/phone-number.ts
+++ validators/phone-number.ts
@@ -2,3 +2,4 @@ import { createValidator } from '../validate.ts';
 import { isPhoneNumber } from '../behaviors/phone-number.ts';
 
 export const IsPhoneNumber = createValidator(isPhoneNumber, 'String is not a local phone number from $constraint1');
+export const IsInternationalPhoneNumber = createValidator(isPhoneNumber, 'String is not an international phone number');
