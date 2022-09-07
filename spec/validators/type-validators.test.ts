01c7f2ab5c2169a9e675dfdcdfbc3e7a8a5aa3fe
Sorikairo <sorikairox@gmail.com>
2022-09-07 13:05:25 +0900

    feat:  isArray

--- spec/validators/type-validators.test.ts
+++ spec/validators/type-validators.test.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString } from '../../validators/types-validators.ts';
+import { IsArray, IsNumber, IsString } from '../../validators/types-validators.ts';
 import { validateObject } from '../../validate.ts';
 import { assertEquals } from 'https://deno.land/std@0.135.0/testing/asserts.ts';
 
@@ -8,10 +8,13 @@ class User {
 
 	@IsNumber()
 	age?: number;
+
+	@IsArray()
+	possibleArray?: any[];
 }
 
 Deno.test('Type validator', () => {
-	const wrongUserDataFromOutsideOurApp = { name: 3, age: 'toto' };
+	const wrongUserDataFromOutsideOurApp = { name: 3, age: 'toto', possibleArray: 'nop' };
 	assertEquals(
 		validateObject(wrongUserDataFromOutsideOurApp, User),
 		[{
@@ -22,6 +25,11 @@ Deno.test('Type validator', () => {
 			property: 'age',
 			errorMessage: `Property must be a number`,
 			constraints: [],
-		}],
+		},
+			{
+				property: 'possibleArray',
+				errorMessage: `Property is not an array`,
+				constraints: [],
+			}],
 	);
 });
--- validators/types-validators.ts
+++ validators/types-validators.ts
@@ -1,4 +1,4 @@
-import { createTypeValidator } from '../validate.ts';
+import { createTypeValidator, createValidator } from '../validate.ts';
 
 export const IsString = createTypeValidator('string');
 export const IsNumber = createTypeValidator('number');
@@ -8,3 +8,4 @@ export const IsFunction = createTypeValidator('function');
 export const IsObject = createTypeValidator('object');
 export const IsSymbol = createTypeValidator('symbol');
 export const IsUndefined = createTypeValidator('undefined');
+export const IsArray = createValidator(() => (prop: any) => Array.isArray(prop), 'Property is not an array');
