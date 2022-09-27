2fd57b19cbecbd7c11b488a2e5c5faef1de445a1
Sorikairo <sorikairox@gmail.com>
2022-09-27 18:50:43 +0900

    feat: test all previously created decorators

---
+++ spec/validators/common.test.ts
@@ -0,0 +1,226 @@
+/* we only test error messages, we trust /spec/behaviors test for behaviors */
+
+import {
+	ByteLength,
+	Contains,
+	IsAfter,
+	IsAlpha,
+	IsAlphaNumeric,
+	IsAscii,
+	IsBase32,
+	IsBase64,
+	IsBefore, IsBic, IsBoolean, IsBtcAddress, IsCreditCard, IsCurrency, IsDataURI, IsDate, IsDecimal
+} from '../../validators/common.ts';
+import { constraintKey, validateObject } from '../../validate.ts';
+import { assertArrayIncludes, fail } from 'https://deno.land/std@0.135.0/testing/asserts.ts';
+import { defaultContainsOptions } from '../../behaviors/common/contains.ts';
+
+class BodyPayload {
+	@Contains('lookingforthis')
+	public contains!: string;
+
+	@IsAfter()
+	public isAfter!: string;
+
+	@IsAlpha()
+	public isAlpha!: string;
+
+	@IsAlphaNumeric()
+	public isAlphaNumeric!: string;
+
+	@IsAscii()
+	public isAscii!: string;
+
+	@IsBase32()
+	public isBase32!: string;
+
+	@IsBase64()
+	public isBase64!: string;
+
+	@IsBefore()
+	public isBefore!: string;
+
+	@IsBic()
+	public isBic!: string;
+
+	@IsBoolean()
+	public isBoolean!: string;
+
+	@IsBtcAddress()
+	public isBtcAddress!: string;
+	@ByteLength({ min: 1, max: 5 })
+	public byteLength!: string;
+	@IsCreditCard()
+	public isCreditCard!: string;
+	@IsCurrency()
+	public isCurrency!: string;
+	@IsDataURI()
+	public isDataURI!: string;
+	@IsDate()
+	public isDate!: string;
+	@IsDecimal()
+	public isDecimal!: string;
+}
+
+Deno.test('Common validators errors', async (ctx) => {
+	const failingPayload = new BodyPayload();
+
+	failingPayload.contains = 'notcontainingit';
+	failingPayload.isAfter = `${Date.now() - 10}`;
+	failingPayload.isAlpha = '123123';
+	failingPayload.isAlphaNumeric = '/./,.,.';
+	failingPayload.isAscii = '日本人';
+	failingPayload.isBase32 = 'nonbase32';
+	failingPayload.isBase64 = 'nonbase64';
+	failingPayload.isBefore = `${Date.now() + 10}`;
+	failingPayload.isBic = 'nonbase64';
+	failingPayload.isBoolean = 'nonbase64';
+	failingPayload.isBtcAddress = 'nonbase64';
+	failingPayload.byteLength = 'tooloooooong';
+	failingPayload.isCreditCard = '234234';
+	failingPayload.isCurrency = 'nonCunrrecy';
+	failingPayload.isDataURI = 'nonDataURI';
+	failingPayload.isDate = 'nonDate';
+	failingPayload.isDecimal = 'nondecimla';
+
+	const errors = validateObject(failingPayload, BodyPayload);
+	await ctx.step('Contains', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must contain 'lookingforthis'`,
+			constraints: ['lookingforthis', defaultContainsOptions],
+			property: 'contains',
+		}]);
+	});
+	await ctx.step('IsAfter', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a date in the future`,
+			constraints: [],
+			property: 'isAfter',
+		}]);
+	});
+	await ctx.step('IsAlpha', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be an alpha string (containing only letters)`,
+			constraints: ['en-US'],
+			property: 'isAlpha',
+		}]);
+	});
+	await ctx.step('IsAlphaNumeric', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be an alphanumeric string (numbers and letters)`,
+			constraints: ['en-US'],
+			property: 'isAlphaNumeric',
+		}]);
+	});
+	await ctx.step('IsAscii', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must contain only ascii characters`,
+			constraints: [],
+			property: 'isAscii',
+		}]);
+	});
+	await ctx.step('IsBase32', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must contain be base32`,
+			constraints: [],
+			property: 'isBase32',
+		}]);
+	});
+	await ctx.step('IsBase64', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must contain be base64`,
+			constraints: [],
+			property: 'isBase64',
+		}]);
+	});
+	await ctx.step('IsBefore', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a date string in the future`,
+			constraints: [],
+			property: 'isBefore',
+		}]);
+	});
+	await ctx.step('IsBic', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a BIC`,
+			constraints: [],
+			property: 'isBic',
+		}]);
+	});
+	await ctx.step('IsBoolean', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a boolean string such as "true", "false", "0", "1"`,
+			constraints: [],
+			property: 'isBoolean',
+		}]);
+	});
+	await ctx.step('IsBtcAddress', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a BTC address`,
+			constraints: [],
+			property: 'isBtcAddress',
+		}]);
+	});
+	await ctx.step('ByteLength', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage:  `Property must have a byte length between 1 and 5`,
+			constraints: [ { min: 1, max: 5 }],
+			property: 'byteLength',
+		}]);
+	});
+	await ctx.step('IsCreditCard', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a valid credit card number`,
+			constraints: [],
+			property: 'isCreditCard',
+		}]);
+	});
+	await ctx.step('IsCurrency', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a currency value`,
+			constraints: [ {
+				symbol: '$',
+				require_symbol: false,
+				allow_space_after_symbol: false,
+				symbol_after_digits: false,
+				allow_negatives: true,
+				parens_for_negatives: false,
+				negative_sign_before_digits: false,
+				negative_sign_after_digits: false,
+				allow_negative_sign_placeholder: false,
+				thousands_separator: ',',
+				decimal_separator: '.',
+				allow_decimal: true,
+				require_decimal: false,
+				digits_after_decimal: [2],
+				allow_space_after_digits: false,
+			}],
+			property: 'isCurrency',
+		}]);
+	});
+	await ctx.step('IsDataURI', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a dataURI string`,
+			constraints: [],
+			property: 'isDataURI',
+		}]);
+	});
+	await ctx.step('IsDate', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a date with format YYYY/MM/DD`,
+			constraints: ['YYYY/MM/DD'],
+			property: 'isDate',
+		}]);
+	});
+	await ctx.step('IsDecimal', () => {
+		assertArrayIncludes(errors, [{
+			errorMessage: `Property must be a decimal string for en-US locale`,
+			constraints: [{
+				force_decimal: false,
+				decimal_digits: '1,',
+				locale: 'en-US',
+			}],
+			property: 'isDecimal',
+		}]);
+	});
+});
--- validators/common.ts
+++ validators/common.ts
@@ -18,32 +18,32 @@ import {
 	isDecimal,
 } from '../behaviors/common/mod.ts';
 import { IsBase64Options } from '../behaviors/common/isBase64.ts';
-import { defaultIsByteLengthOptions, IsByteLengthOptions } from '../behaviors/common/isByteLength.ts';
+import { defaultIsByteLengthOptions } from '../behaviors/common/isByteLength.ts';
 import { defaultCurrencyOptions } from '../behaviors/common/isCurrency.ts';
 import { defaultDecimalOptions } from '../behaviors/common/isDecimal.ts';
 import { contains, defaultContainsOptions } from '../behaviors/common/contains.ts';
 
 export const Contains = (seed: string, options = defaultContainsOptions) =>
 	createDecorator((prop: string) => contains(prop, seed, options), {
-		errorMessage: `Property must contain ${seed}`,
+		errorMessage: `Property must contain '${seed}'`,
 		constraints: [seed, options],
 	});
 
 export const IsAfter = (dateToBeAfter?: string) =>
 	createDecorator((prop: string) => isAfter(prop, dateToBeAfter), {
-		errorMessage: `Property must be a date later than ${dateToBeAfter}`,
-		constraints: [dateToBeAfter],
+		errorMessage: `Property must be a date ${dateToBeAfter ? `later than ${dateToBeAfter}` : 'in the future'}`,
+		constraints: dateToBeAfter ? [dateToBeAfter] : [],
 	});
 
 export const IsAlpha = (locale = 'en-US') =>
 	createDecorator((prop: string) => isAlpha(prop, locale), {
-		errorMessage: `Property must be alpha`,
+		errorMessage: `Property must be an alpha string (containing only letters)`,
 		constraints: [locale],
 	});
 
 export const IsAlphaNumeric = (locale = 'en-US') =>
 	createDecorator((prop: string) => isAlphanumeric(prop, locale), {
-		errorMessage: `Property must be alphanumeric`,
+		errorMessage: `Property must be an alphanumeric string (numbers and letters)`,
 		constraints: [locale],
 	});
 
@@ -62,41 +62,45 @@ export const IsBase32 = () =>
 export const IsBase64 = (option?: IsBase64Options) =>
 	createDecorator((prop: string) => isBase64(prop, option), {
 		errorMessage: `Property must contain be base64`,
-		constraints: [],
+		constraints: option ? [option] : [],
 	});
 
 export const IsBefore = (dateToBeBefore?: string) =>
 	createDecorator((prop: string) => isBefore(prop, dateToBeBefore), {
-		errorMessage: `Property must be a date string before ${constraintKey}1`,
-		constraints: [dateToBeBefore],
+		errorMessage: `Property must be a date string ${dateToBeBefore ? `before ${dateToBeBefore}`: 'in the future'}`,
+		constraints: dateToBeBefore ? [dateToBeBefore] : [],
 	});
 
 export const IsBic = () =>
 	createDecorator((prop: string) => isBIC(prop), {
 		errorMessage: 'Property must be a BIC',
+		constraints: [],
 	});
 
 export const IsBoolean = () =>
 	createDecorator((prop: string) => isBoolean(prop), {
 		errorMessage: 'Property must be a boolean string such as "true", "false", "0", "1"',
+		constraints: [],
 	});
 
 export const IsBtcAddress = () =>
 	createDecorator((prop: string) => isBtcAddress(prop), {
 		errorMessage: 'Property must be a BTC address',
+		constraints: [],
 	});
 
 export const ByteLength = (options = defaultIsByteLengthOptions) =>
 	createDecorator((prop: string) => isByteLength(prop, options), {
-		errorMessage: `Property must have a byte length between ${constraintKey}1 and ${
-			options.max ? `${constraintKey}1` : 'infinity'
+		errorMessage: `Property must have a byte length between ${options.min ?? 0} and ${
+			options.max ?? 'infinity'
 		}`,
-		constraints: [options.min, options.max],
+		constraints: [options],
 	});
 
 export const IsCreditCard = () =>
 	createDecorator((prop: string) => isCreditCard(prop), {
 		errorMessage: 'Property must be a valid credit card number',
+		constraints: [],
 	});
 
 export const IsCurrency = (options = defaultCurrencyOptions) =>
@@ -108,11 +112,12 @@ export const IsCurrency = (options = defaultCurrencyOptions) =>
 export const IsDataURI = () =>
 	createDecorator((prop: string) => isDataURI(prop), {
 		errorMessage: 'Property must be a dataURI string',
+		constraints: [],
 	});
 
 export const IsDate = (format = 'YYYY/MM/DD') =>
 	createDecorator((prop: string) => isDate(prop, format), {
-		errorMessage: `Property must be a date ${constraintKey}1`,
+		errorMessage: `Property must be a date with format ${format}`,
 		constraints: [format],
 	});
 
